3666e85ac738076e47a36c836c5fd666c19320ba
bmagalhaes <60456840+bmagalhaes@users.noreply.github.com>
2020-02-25 14:00:49 -0600

    Create hansen_dwi.R

---
+++ Do/hansen_dwi.R
@@ -0,0 +1,6 @@
+###################################################
+############# Hansen DUI Replication ##############
+###################################################
+
+# read in the data
+dwi = read.csv(url("https://raw.githubusercontent.com/bmagalhaes/Hansen-DUI/master/Data/hansen_dwi.csv"))
